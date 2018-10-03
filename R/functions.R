cdfe6d67ae033a10a99ce5fcc0223bb9b7ab44e3
PeerChristensen <hr.pchristensen@gmail.com>
2018-10-03 15:53:18 +0200

    Initial commit

---
+++ R/functions.R
@@ -0,0 +1,95 @@
+#' Main function at user level
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+#' @param base_url url provided by user
+#' @param page_lim number of pages to parse
+#' @param company  company name
+#' @param verbose  whether to print current url
+#'
+#' @export
+#'
+#' @import rvest
+#' @import dplyr
+#' @import lubridate
+#' @import stringr
+#' @import purrr
+#'
+#' @examples
+#' get_reviews("https://dk.trustpilot.com/review/trustpilot.com",5,"trustpilot")
+
+
+### MAIN FUNCTION: PREPARE REVIEW DATASET ###
+
+get_reviews <- function(base_url, page_lim = NULL, company = NULL, verbose = TRUE) {
+
+
+  ### NESTED FUNCTION ###
+
+  build_dfs <- function(url) {
+
+    # whether to print the current url
+    if (verbose == TRUE) {
+      message(url)
+    }
+    # get HTML
+    html <- url %>% xml2::read_html()
+
+    # get id
+    id <- html %>% get_id()
+
+    # get time
+    time <- html %>% get_time()
+
+    # get rating
+    rating <- html %>% get_rating()
+
+    # get review
+    review <- html %>% get_review_text()
+    review <- as.character(review)
+
+    # gather variables in tibble
+    tibble(id,time,rating,review)
+  }
+
+  ### START OF MAIN FUNCTION ###
+
+  # if page limit is set, do..
+  if (!is.null(page_lim)) {
+    urls <- get_pages(base_url)
+    urls <- urls[1:page_lim]
+  }
+
+  else if (is.null(page_lim)) {
+    urls <- get_pages(base_url)
+  }
+
+  # if company name is set, do..
+  if (is.null(company)) {
+    data <- urls %>% map(build_dfs)
+    data <- do.call("rbind",data)
+  }
+
+  # if company name is not set, do..
+  else if (!is.null(company)) {
+    data <- urls %>% map(build_dfs)
+    data <- do.call("rbind",data) %>%
+      mutate(company = company) %>%
+      select(company, everything())
+  }
+}
+
+# example
+#base_url = "https://dk.trustpilot.com/review/www.3.dk"
+#d = get_reviews(base_url,6,verbose=T)
+#d
+
+
---
+++ R/scraper_functions.R
@@ -0,0 +1,86 @@
+#' Functions for scraping data
+#'
+#'
+#' @param base_url url provided by user
+#'
+#' @export
+#'
+#' @import rvest
+#' @import dplyr
+#' @import lubridate
+#' @import stringr
+
+
+### FUNCTION: GET N PAGES ###
+
+n_pages <- function(base_url){
+
+  base_url                         %>%
+    xml2::read_html()                    %>%
+    html_nodes(".pagination-page") %>%
+    html_text()                    %>%
+    as.numeric()                   %>%
+    max(na.rm=T)
+}
+
+### FUNCTION: GET ALL PAGES ###
+
+get_pages <- function(base_url){
+
+  last_page    = n_pages(base_url)
+  page_numbers = seq_along(1:last_page)
+  urls         = paste0(base_url,"?page=", page_numbers)
+}
+
+### FUNCTION: GET TIME ###
+
+get_time <- function(html) {
+
+  time = html             %>%
+    html_nodes("time")    %>%
+    html_attrs()          %>%
+    map(1)                %>%
+    unlist()              %>%
+    date()
+
+  class = html            %>%
+    html_nodes("time")    %>%
+    html_attrs()          %>%
+    map(2)
+
+  data.frame(time,class)     %>%
+    filter(class == "ndate") %>%
+    select(time)             %>%
+    pull(time)               %>%
+    tail(20)
+}
+
+### FUNCTION: GET ID ###
+
+get_id <- function(html) {
+
+  html                                    %>%
+    html_nodes("consumer-review-picture") %>%
+    html_attr("consumer-display-name")
+}
+
+### FUNCTION: GET RATING ###
+
+get_rating <- function(html) {
+
+  html                         %>%
+    html_nodes(".star-rating") %>%
+    html_attrs()               %>%
+    str_extract("[:digit:]")   %>%
+    as.numeric()               %>%
+    tail(20)
+}
+
+### FUNCTION: GET REVIEW TEXT ###
+
+get_review_text <- function(html) {
+
+  html                                     %>%
+    html_nodes(".review-info__body__text") %>%
+    html_text(trim=T)
+}
